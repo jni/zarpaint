a37a5b238aa359b168c09071ad62a0ae6ffc0f7d
Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>
2021-08-06 18:44:56 +1000

    Add click callback for adding points in 3D (#13)
    * Add click callback for adding points in 3D
    
    * Fix off by one error in computing coordinates
    
    * Use forward refs to avoid lazy-import errors
    
    * Check for None in the returned world coordinates

--- zarpaint/plugin.py
+++ zarpaint/plugin.py
@@ -1,11 +1,14 @@
 from __future__ import annotations
 
 import os
+import magicgui
+import napari
 import pathlib
 from napari_plugin_engine import napari_hook_implementation
 from ._zarpaint import create_labels, open_tensorstore, open_ts_meta
 from ._dims_chooser import DimsSorter, set_axis_labels
 from ._watershed import watershed_split
+from ._add_3d_points import find_midpoint_of_first_segment
 
 
 @napari_hook_implementation
@@ -41,3 +44,31 @@ def zarr_tensorstore(path: str | pathlib.Path):
     if (str(path).endswith('.zarr')
             and os.path.isdir(path) and '.zarray' in os.listdir(path)):
         return lambda path: [(open_tensorstore(path), open_ts_meta(path), 'labels')]
+
+
+@napari_hook_implementation(
+    specname='napari_experimental_provide_dock_widget'
+)
+def _add_points_callback():
+
+    @magicgui.magic_factory
+    def add_points_3d_with_alt_click(
+        labels: napari.layers.Labels,
+        points: napari.layers.Points,
+    ):
+        pts_world2data = points._transforms[1:3].simplified.inverse
+
+        @labels.mouse_drag_callbacks.append
+        def click_callback(layer, event):
+            if not (
+                len(event.modifiers) == 1
+                and event.modifiers[0].name == 'Alt'
+            ):
+                return
+            world_click_coordinates = find_midpoint_of_first_segment(
+                layer, event
+            )
+            if world_click_coordinates is not None:
+                pts_coordinates = pts_world2data(world_click_coordinates)
+                points.add(pts_coordinates)
+    return add_points_3d_with_alt_click
