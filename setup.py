dfe24827ddebed0f7507584b989279566224fcc5
James Ryan <95660545+jamesyan-git@users.noreply.github.com>
2022-02-28 19:41:56 +1100

    Switch to src layout (#22)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 
 from setuptools import setup
 # https://github.com/pypa/setuptools_scm
-use_scm = {"write_to": "zarpaint/_version.py"}
+use_scm = {"write_to": "src/zarpaint/_version.py"}
 
 setup(
         use_scm_version=use_scm,
