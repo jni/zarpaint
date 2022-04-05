c582d5b8a88a5d767d164b56cfccd190a47d82d8
James Ryan <95660545+jamesyan-git@users.noreply.github.com>
2022-04-06 09:57:02 +1000

    Add reader command to napari.yaml (#25)
    Co-authored-by: Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>

--- src/zarpaint/__init__.py
+++ src/zarpaint/__init__.py
@@ -7,6 +7,7 @@ from ._zarpaint import create_labels, open_tensorstore
 from ._dims_chooser import DimsSorter, set_axis_labels
 from ._watershed import watershed_split
 from ._add_3d_points import add_points_3d_with_alt_click
+from .reader import zarr_tensorstore
 from ._copy_data import copy_data
 
 __all__ = [
@@ -16,5 +17,6 @@ __all__ = [
         'set_axis_labels',
         'watershed_split',
         'add_points_3d_with_alt_click',
+        'zarr_tensorstore'
         'copy_data',
         ]
