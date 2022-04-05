b513a6f9a17e376dcb5bf8467165f9de4ee310ae
Peter Kaltzis <pkal0002@student.monash.edu>
2022-04-06 09:48:16 +1000

    Fix np.zeros call

--- src/zarpaint/_tests/test_copy_data.py
+++ src/zarpaint/_tests/test_copy_data.py
@@ -6,7 +6,7 @@ import numpy as np
 def test_copy_data(make_napari_viewer):
     viewer = make_napari_viewer()
     labels_layer1 = viewer.add_labels(np.random.randint(0, 2**23, size=(10, 20, 30)))
-    labels_layer2 = viewer.add_labels(np.zeros(size=(2, 10, 20, 30)))
+    labels_layer2 = viewer.add_labels(np.zeros((2, 10, 20, 30), dtype=int))
     viewer.dims.set_point(axis=0, value=1)
     widget = copy_data()
     widget(viewer, labels_layer1, labels_layer2)
