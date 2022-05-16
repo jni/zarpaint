613a927c112a7330a0af4b768cb09bdcf90694db
James Ryan <work.jdryan@gmail.com>
2022-05-16 11:25:48 +1000

    added test for 2d watershed split

---
+++ src/zarpaint/_tests/test_watershed.py
@@ -0,0 +1,40 @@
+import numpy as np
+from zarpaint import watershed_split
+
+def test_watershed_split_2d(make_napari_viewer):
+    data = np.zeros(shape=(18,18), dtype="uint8")
+    # create 2 squares with one corner overlapping
+    data[1:10, 1:10] = 1
+    data[8:17, 8:17] = 1
+    print(data)
+
+    # palce points in the centre of the 2 squares
+    points = np.asarray([[5,5], [12,12]])
+
+    expected = np.asarray([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 0],
+       [0, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]],
+      dtype="uint8")
+
+    viewer = make_napari_viewer()
+    viewer.add_labels(data)
+    viewer.add_points(points)
+    watershed_widget = watershed_split()
+    watershed_widget(viewer, viewer.layers[0], viewer.layers[1])
+    np.testing.assert_allclose(viewer.layers[0].data, expected)
+    
