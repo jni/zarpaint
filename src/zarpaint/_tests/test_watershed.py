c5416f0baaafe6c4e4582c8a29bef281c11709b4
James Ryan <work.jdryan@gmail.com>
2022-05-16 14:04:02 +1000

    added basic test for 3d watershed split

--- src/zarpaint/_tests/test_watershed.py
+++ src/zarpaint/_tests/test_watershed.py
@@ -37,4 +37,24 @@ def test_watershed_split_2d(make_napari_viewer):
     watershed_widget = watershed_split()
     watershed_widget(viewer, viewer.layers[0], viewer.layers[1])
     np.testing.assert_allclose(viewer.layers[0].data, expected)
+
+
+
+
     
+def test_watershed_split_3d(make_napari_viewer):
+    data = np.zeros(shape=(9,9,9), dtype="uint8")
+    data[4:,4:,4:] = 1
+    data[:5,:5,:5] = 1
+    viewer = make_napari_viewer()
+    viewer.add_labels(data)
+
+    points = np.asarray([[4,2,2], [4,6,6]])
+    
+    viewer.add_points(points)
+
+    watershed_widget = watershed_split()
+    watershed_widget(viewer, viewer.layers[0], viewer.layers[1])
+
+    assert np.all(data[:5,:5,:5] == 2)
+    assert np.all(data[5:,5:,5:] == 3)
