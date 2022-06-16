46dceb1393c70e5bb744a0491f5d35ba20ab1e45
jamesyan-git <work.jdryan@gmail.com>
2022-06-16 11:10:09 +1000

    basic container widget added

--- src/zarpaint/_interpolate_labels.py
+++ src/zarpaint/_interpolate_labels.py
@@ -1,5 +1,8 @@
 from cProfile import label
+from typing import Sequence
 from magicgui import magic_factory
+from magicgui.widgets import Container, ComboBox
+import napari
 import numpy as np
 from scipy.interpolate import interpn
 from scipy.ndimage import distance_transform_edt
@@ -48,13 +51,62 @@ def interpolated_slice(percent, points, values, interp_dim=0, method='linear'):
     interpolated_img = interpn(points, values, xi, method=method)
     interpolated_img = np.reshape(interpolated_img, img_shape) > 0
     return interpolated_img
-    
+
+# 1. Create labels and widget for each parameter to interpolate_between_slices, and add them to form layout
+# 2. Add run button and connect callback that just prints something
+# 3. Set initial options and boundaries for different widgets e.g. min/max for slice_index
+# 4. Update button callback to actually do the thing
+class InterpolateSliceWidget(Container):
+
+    def __init__(self, viewer: "napari.viewer.Viewer"):
+        super().__init__()
+        self.viewer = viewer
+        self.labels_combo = ComboBox(name='Labels Layer', choices=self.get_labels_layers)
+        self.append(self.labels_combo)
+
+    def get_labels_layers(self, combo):
+        return [layer for layer in self.viewer.layers if isinstance(layer, napari.layers.Labels)]
+
+
+
+    # def __init__(self, viewer: 'napari.viewer.Viewer') -> None:
+    #     super().__init__()
+    #     self.layer_label = "Labels Layer: "
+    #     self.layer_combo = QComboBox()
+    #     viewer.layers.inserted.conecct(self.reset_layer_options)
+    #     viewer.layers.removed.connect(self.reset_layer_options)
+
+    #     self.layer_combo.currentText.connect(self.set_default_label_id)
+        
+
+    #     my_layour = QFormLayout()
+    #     my_layour.addRow(layer_label, layer_combo)
+
+
+    #     self.setLayout(my_layour)
+
+
+def on_init(interpolate_widget):
+    """called each time widget_factory creates a new widget."""
+
+    # at best, this would work just when you first start the widget
+    interpolate_widget.label_id = interpolate_widget.label_layer.selected_label
+
+    @interpolate_widget.label_layer.selection_changed.connect
+    def set_default_label_id(event):
+        print('YOu just selected a new labels layer: ', event.value)
+        interpolate_widget.label_id.value = interpolate_widget.viewer.layers[event.value].selsected_label
+
+
 
 ## Second draft, writes directly into tensorstore zarr array
 @magic_factory(
         call_button='Interpolate'
+        # widget_init=on_init
         )
-def interpolate_between_slices(label_layer: "napari.layers.Labels", slice_index_1: int, slice_index_2: int, label_id: int =1, interp_dim: int =0):
+def interpolate_between_slices(viewer: "napari.viewer.Viewer", label_layer: "napari.layers.Labels", slice_index_1: int, slice_index_2: int, label_id: int =1, interp_dim: int =0):
+    print(viewer.layers)
+    print()
     if slice_index_1 > slice_index_2:
         slice_index_1, slice_index_2 = slice_index_2, slice_index_1
     layer_data = np.asarray(label_layer.data)
