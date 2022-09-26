fc48307b899a36109101c87df1da762cad014d76
jamesyan-git <work.jdryan@gmail.com>
2022-09-27 09:32:43 +1000

    Add docstrings

--- src/zarpaint/_interpolate_labels.py
+++ src/zarpaint/_interpolate_labels.py
@@ -8,10 +8,17 @@ from scipy.ndimage import distance_transform_edt
 
 
 def distance_transform(image):
-    """Distance transform for a boolean image.
-    
-    Returns positive values inside the object,
-    and negative values outside.
+    """apply distance transform and return new image
+
+    Parameters
+    ----------
+    image : np.array
+        the label layer data
+
+    Returns
+    -------
+    np.array
+        label layer data with distance transform applied
     """
     image = image.astype(bool)
     edt = distance_transform_edt(image) - distance_transform_edt(~image)
@@ -19,6 +26,26 @@ def distance_transform(image):
 
 
 def point_and_values(image_1, image_2, interp_dim=0):
+    """apply distance transforms to the 2 images to interpolate.
+
+    Apply distance transforms to the 2 images that are going to be 
+    interpolated between. Return a tuple of points and values where
+    points represent  
+
+    Parameters
+    ----------
+    image_1 : np.array
+        The first slice to interpolate from
+    image_2 : _type_
+        The second slice to interpolate to
+    interp_dim : int, optional
+        The dimention along which to interpolate, by default 0
+
+    Returns
+    -------
+    tuple of numpy array
+        tuple og distance transform data and corresponding location
+    """
     edt_1 = distance_transform(image_1)
     edt_2 = distance_transform(image_2)
     values = np.stack([edt_1, edt_2], axis=interp_dim)
@@ -27,6 +54,22 @@ def point_and_values(image_1, image_2, interp_dim=0):
 
 
 def xi_coords(shape, percent=0.5, interp_dim=0):
+    """ 
+    create array of coordinates to interpolate between
+    Parameters
+    ----------
+    shape : tuple
+        Shape of the slice
+    percent : float, optional
+        Value to populate the xi array, by default 0.5
+    interp_dim : int, optional
+        The axis to interpolate along , by default 0
+
+    Returns
+    -------
+    numpy array
+        Coordinate denoting the area of the slice to interpolate along
+    """
     slices = [slice(0, i) for i in shape]
     xi = np.moveaxis(np.mgrid[slices], 0,
                      -1).reshape(np.prod(shape), len(shape)).astype('float')
@@ -35,14 +78,49 @@ def xi_coords(shape, percent=0.5, interp_dim=0):
 
 
 def slice_iterator(slice_index_1, slice_index_2):
+    """create an iterable across the range of slices to be interpolated
+
+    Parameters
+    ----------
+    slice_index_1 : int
+        number of one bound of the slice range
+    slice_index_2 : _type_
+        the opposite bound of the slice range
+
+    Returns
+    -------
+    zip of 2 numpy arrays
+        tuple of slice indicies, tuple of percentages to give xi coords
+    """
     intermediate_slices = np.arange(slice_index_1 + 1, slice_index_2)
-    n_slices = slice_index_2 - slice_index_1 + 1  # inclusive
+    n_slices = slice_index_2 - slice_index_1 + 1
     stepsize = 1 / n_slices
     intermediate_percentages = np.arange(0 + stepsize, 1, stepsize)
     return zip(intermediate_slices, intermediate_percentages)
 
 
 def interpolated_slice(percent, points, values, interp_dim=0, method='linear'):
+    """Create the dtata for one of the interpolated slices
+
+    Parameters
+    ----------
+    percent : array_like
+        A value to populate the xi array
+    points : tuple of ndarray of float
+        The points of the slice on which to paint
+    values : array_like
+        Data to draw on the slice
+    interp_dim : int, optional
+        The axis along which to interpolate, by default 0
+    method : str, optional
+        Interpolation method, by default 'linear'
+
+    Returns
+    -------
+    np array 
+        A slice with interpolated data drawn on
+    """
+    # TODO: check return type
     img_shape = list(values.shape)
     del img_shape[interp_dim]
 
@@ -54,6 +132,13 @@ def interpolated_slice(percent, points, values, interp_dim=0, method='linear'):
 
 class InterpolateSliceWidget(Container):
     def __init__(self, viewer: "napari.viewer.Viewer"):
+        """Widget for handling the interpolate slice gui and event callbacks
+
+        Parameters
+        ----------
+        viewer : napari.viewer.Viewer
+            napari viewer to add the widget to
+        """
         super().__init__()
         self.viewer = viewer
         self.labels_combo = ComboBox(
@@ -71,12 +156,31 @@ class InterpolateSliceWidget(Container):
         self.interp_dim = None
 
     def get_labels_layers(self, combo):
+        """Returns a list of existing labels to display
+
+        Parameters
+        ----------
+        combo : magicgui ComboBox
+            A dropdown to dispaly the layers
+
+        Returns
+        -------
+        list[napari.layer.label]
+            A list of curently existing layers
+        """
         return [
                 layer for layer in self.viewer.layers
                 if isinstance(layer, napari.layers.Labels)
                 ]
 
     def paint_callback(self, event):
+        """Identify slices that have been painetd on
+
+        Parameters
+        ----------
+        event : Event
+            napari paint event
+        """
         last_label_history_item = event.value
         real_item = []
         # filter empty history atoms from item
@@ -108,6 +212,19 @@ class InterpolateSliceWidget(Container):
         self.painted_slice_history[label_id].add(last_slice_painted)
 
     def _infer_interp_dim(self, unique_coords):
+        """Infer the dimension/axis on which to interpolate.
+
+        unique_coords contains a list for each dimension.
+        One of the lists will contain a single element referencing the slice
+        being painted on. This mean that that lists is the dimension being
+        painted across
+
+        Parameters
+        ----------
+        unique_coords : List
+            A list of lists, containing coordinates which have been painted
+            and the label which is being painted
+        """
         interp_dim = None
         for i in range(len(unique_coords)):
             if len(unique_coords[i]) == 1:
@@ -122,8 +239,13 @@ class InterpolateSliceWidget(Container):
             self.interp_dim = interp_dim
 
     def enter_interpolation(self, event):
-        # TODO: we wanna connect some callbacks that track for us the painted labels
-        # grey out the combo box
+        """Connect the paint callback and change button text
+
+        Parameters
+        ----------
+        event : Event
+            Event spawned by button click
+        """
         self.selected_layer = self.viewer.layers[
                 self.labels_combo.current_choice]
 
@@ -135,7 +257,14 @@ class InterpolateSliceWidget(Container):
         self.interpolate_btn.clicked.connect(self.interpolate)
 
     def interpolate(self, event):
+        """For each label_id, iterate over each slice that has been painted on
+         and perform pairwise (i, i+1) interpolation on each pair.
 
+        Parameters
+        ----------
+        event : Event
+            Object created upon clicking "interpolate" in the widget
+        """
         assert self.interp_dim is not None, 'Cannot interpolate without knowing dimension'
 
         for label_id, slices_painted in self.painted_slice_history.items():
@@ -150,6 +279,8 @@ class InterpolateSliceWidget(Container):
         self.reset()
 
     def reset(self):
+        """Reset button text and clear paint event history
+        """
         self.selected_layer.events.paint.disconnect(self.paint_callback)
         self.painted_slice_history.clear()
         self.interp_dim = None
@@ -166,6 +297,22 @@ def interpolate_between_slices(
         label_id: int = 1,
         interp_dim: int = 0
         ):
+    """Compute and draw interpolation between 2 label annotations.
+
+    Parameters
+    ----------
+    label_layer : napari.layers.Labels
+        The label layer to draw on 
+    slice_index_1 : int
+        slice containing the first label annotation
+    slice_index_2 : int
+        slice containing the second label anotation
+        interpolation occurs between slice_index_1 slice_index_2
+    label_id : int, optional
+        the id of the annotation that is to be painted, by default 1
+    interp_dim : int, optional
+        the dimension/axis to interpolate across, by default 0
+    """
 
     if slice_index_1 > slice_index_2:
         slice_index_1, slice_index_2 = slice_index_2, slice_index_1
@@ -190,7 +337,5 @@ def interpolate_between_slices(
         indices = [slice(None) for _ in range(label_layer.data.ndim)]
         indices[interp_dim] = slice_number
         indices = tuple(indices)
-        label_layer.data[indices][
-                interpolated_img
-                ] = label_id  #use labels data_setitem (from the paint event PR)
-    label_layer.refresh()  # will update the current view
+        label_layer.data[indices][interpolated_img] = label_id
+    label_layer.refresh()
