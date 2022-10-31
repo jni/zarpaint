61d92b919863f065606c1630a704a0b57b99a05c
Lencripe <dlamz255@outlook.com>
2022-10-31 16:39:21 +1100

    Removing tensore funtionality.

--- src/zarpaint/reader.py
+++ src/zarpaint/reader.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-
+import zarr
 import os
 import pathlib
 from ._zarpaint import open_tensorstore, open_ts_meta
@@ -9,5 +9,5 @@ def zarr_tensorstore(path: str | pathlib.Path):
     if (str(path).endswith('.zarr') and os.path.isdir(path)
                 and '.zarray' in os.listdir(path)):
         return lambda path: [
-                (open_tensorstore(path), open_ts_meta(path), 'labels')
+                (  zarr.open(path), open_ts_meta(path), 'labels')
                 ]
